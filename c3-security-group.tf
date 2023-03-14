9eb2e7cd9c0acc2122e25837d964b99b363e9350
rln <rln@gmail.com>
2023-03-14 11:40:25 +0530

    Terraform-code

--- c3-security-group.tf
+++ c3-security-group.tf
@@ -1,1 +1,77 @@
+#SG for Tomcat-server
+resource "aws_security_group" "Tomcat-server-sg" {
+  name        = "Tomcat-server-sg"
+  description = "SG for Tomcat-server"
+  ingress {
+    description      = "Allow port 22"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Allow port 8089"
+    from_port        = 8089
+    to_port          = 8089
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Allow port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
+  egress {
+    description      = "Allow all ip and ports outbound"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Tomcat-server-SG"
+  }
+}
+
+#SG for Jenkins server
+resource "aws_security_group" "Jenkins-server-sg" { 
+  name        = "My-vpc"
+  description = "My-vpc for ssh"
+  ingress {
+    description      = "Allow port 22"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Allow port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Allow port 8080"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow all ip and ports outbound"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-server-sg"
+  }
+}
--- c5-ec2-instance.tf
+++ c5-ec2-instance.tf
@@ -1,1 +1,28 @@
+# Tomcat-server-instance 
+# EC2 Instance
+
+resource "aws_instance" "Tomcat-Server" {
+    ami = data.aws_ami.amzlinux2.id
+    instance_type = var.instance_type
+    user_data = file("${path.module}/setup-tomcat-server.sh")
+    key_name = var.instance_keypair
+    vpc_security_group_ids = [aws_security_group.Tomcat-server-sg.id]
+    tags = {
+        "Name" = "Tomcat-Server"
+    } 
+}
+
+# Jenkins-Server
+# EC2 Instance
+
+resource "aws_instance" "Jenkins-Server" {
+    ami = data.aws_ami.amzlinux2.id
+    instance_type = var.instance_type
+    user_data = file("${path.module}/jenkins-server.sh")
+    key_name = var.instance_keypair
+    vpc_security_group_ids = [aws_security_group.Jenkins-server-sg.id]
+    tags = {
+        "Name" = "Jenkins-Server"
+    } 
+}
 
